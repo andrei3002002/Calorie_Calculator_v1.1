c6a1812e96664d26e37f6a7a6f2eb67ad6a199ba
Andrei Vilcu <andrei3002002@gmail.com>
2023-09-04 18:35:12 +0200

    creat class Product

--- src/DataProduct/Product.java
+++ src/DataProduct/Product.java
@@ -1,7 +1,27 @@
-package DataProduct;
-
-
+/**
+ * Класс {@code Product} представляет собой продукт с названием и калорийностью.
+ */
 public class Product {
 
+  private String name;
+  private double calories;
 
+  /**
+   * Создает новый экземпляр продукта с указанными названием и калорийностью.
+   *
+   * @param name     Название продукта.
+   * @param calories Калорийность продукта.
+   */
+  public Product(String name, double calories) {
+    this.name = name;
+    this.calories = calories;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public double getCalories() {
+    return calories;
+  }
 }
