c2bf3b3a2378d61180d55bd0adb730327cc51a96
Andrei Vilcu <andrei3002002@gmail.com>
2023-09-04 18:43:15 +0200

    creat class Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
 
   public static void main(String[] args) {
-
-
+    UserInterface userInterface = new UserInterface();
+    System.out.println("\n\t\t\t*************************");
+    System.out.println("\t\t\t*  \033[1;92mКалькулятор калорий\033[0m  *");
+    System.out.println("\t\t\t*************************");
+    userInterface.run();
   }
 }
