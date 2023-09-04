69a201269fce8b62014cf68ec3d4a8c15742d857
Andrei Vilcu <andrei3002002@gmail.com>
2023-09-04 18:42:38 +0200

    creat class Calculator

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -1,7 +1,20 @@
-package Calculator;
-
+import java.util.List;
 
 public class Calculator {
 
-
+  /**
+   * Рассчитывает общую калорийность на основе списка продуктов и их весов.
+   *
+   * @param products Список продуктов.
+   * @param weights  Список весов для каждого продукта. Каждый вес соответствует продукту в списке
+   *                 продуктов.
+   * @return Общая калорийность продуктов, учитывая их вес.
+   */
+  public double calculateTotalCalories(List<Product> products, List<Double> weights) {
+    double totalCalories = 0;
+    for (int i = 0; i < products.size(); i++) {
+      totalCalories += products.get(i).getCalories() * weights.get(i) / 100;
+    }
+    return totalCalories;
+  }
 }
